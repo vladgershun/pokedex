30a8c7b18c311bafe7cbe73deedfd95f715f5a5f
Vlad Gershun <dev.vladgershun@gmail.com>
2022-12-13 16:13:51 -0800

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Vlad Gershun on 12/13/22.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
